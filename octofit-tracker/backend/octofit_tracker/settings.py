bef7a991c5ac829e362827875722b2c22381f3ad
Kelvin Wangonya <kwangonya@gmail.com>
2025-09-04 14:01:28 +0000

    Update settings.py and urls.py for Codespace and localhost REST API endpoint support

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-yng4_9p6il@24wa(zd_(h(pw5&ipvk98@9e)%zu%rha#)7(uz$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f"{CODESPACE_NAME}-8000.app.github.dev")
 
 
 # Application definition
